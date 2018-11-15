b087ef8c70e9247852779e62bc4ecc6be96ed8ce
PSzczepanski1996 <p.szczepanski996@gmail.com>
2018-11-15 17:09:03 +0100

    Vaporize it

--- index.php
+++ index.php
@@ -5,12 +5,8 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <link href='style.css' type='text/css' rel='stylesheet'>
         <meta charset="UTF-8">
-        <style>
-            .container-fluid{
-                padding: 0px;
-            }
-        </style>
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-info">
@@ -25,6 +21,25 @@
                         for($i = 2; $i < $dir_amount; $i++){
                             print('<li class="list-group-item">');
                             print('<a href="builds/'.$dir_listing[$i].'">'.$dir_listing[$i].'</a>');
+                            $shorten_fname = explode('.', $dir_listing[$i])[0];
+                            $created_path = 'build_info/'.$shorten_fname.'.info';
+                            if(file_exists($created_path)){
+                                $build_info = parse_ini_file($created_path, true);
+                                if(isset($build_info['build_details']['version']))
+                                    $version = $build_info['build_details']['version'];
+                                if(isset($build_info['build_details']['compiler']))
+                                    $compiler = $build_info['build_details']['compiler'];
+                                if(isset($version) || isset($compiler)){
+                                    print('<div class="info-bar">');
+                                    if(isset($version)){
+                                        print('<div class="ver-info">Ver: '.$version.'</div>');
+                                    }
+                                    if(isset($compiler)){
+                                        print('<div class="compiler">compiled by '.$compiler.'</div>');
+                                    }
+                                    print('</div>');
+                                }
+                            }
                             print('</li>');
                         }
                     } else {
